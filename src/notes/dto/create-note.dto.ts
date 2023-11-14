1dd623dabe28a7c77d646693cdcdd44089352a2e
ansthsys <azmiythufail91@gmail.com>
2023-11-14 10:54:03 +0700

    feat: add input validation

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,12 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(200)
   @ApiProperty()
   content: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: true })
   status: boolean = true;
 }
